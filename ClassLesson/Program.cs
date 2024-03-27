9a59a34a8bd5d1c5a1061dc49cf206508d6156ef
unknown <you@example.com>
2024-03-28 01:49:43 +0400

    added Class

---
+++ ClassLesson/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLesson.classes;
+
+namespace ClassLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Qezenfer", "aliyev", "ab107", 91, true);
+
+                //student1.Getİnfo();
+            //student1.CheckGraduation();
+             //student1.GetDiplomDegree();
+        }
+    }
+}
---
+++ ClassLesson/classes/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLesson.classes
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public byte Point;
+        public bool isGraduated;
+    
+   public Student(string name, string surname,string group,byte point,bool isgraduated)
+    {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            isGraduated = isgraduated && point >= 65;
+    }
+        public void Getİnfo()
+        {
+            Console.WriteLine($"ad:{Name}");
+            Console.WriteLine($"soyad:{Surname}");
+            Console.WriteLine($"qrup:{Group}");
+            Console.WriteLine($"bal:{Point}");
+            Console.WriteLine($"mezun olub olmadigi:{(isGraduated ? "mezun" : "mezun deyil")}");
+        }
+        public void CheckGraduation()
+        {
+            if (isGraduated) Console.WriteLine("mezundur");
+            else
+            {
+                Console.WriteLine("mezun deyil");
+            }
+        }
+        public void GetDiplomDegree()
+        {
+            if (Point < 65) Console.WriteLine("yoxdur");
+            else if (Point <= 80 && Point > 65) Console.WriteLine("adi");
+            else if (Point <= 90 && Point > 80) Console.WriteLine("seref");
+            else Console.WriteLine("yuksek  seref");    
+        }
+}
+}
